1e94810a433e103350e1a6387f7c7ef0a2e4603c
Ubuntu <Team-205@Team-205v2.eorvuw12extuvcx4uphwvmlkna.bx.internal.cloudapp.net>
2022-10-21 20:49:24 +0000

    Setup NGINX, add steps to `install.sh` add `.conf` files

--- client/install.sh
+++ client/install.sh
@@ -8,10 +8,40 @@ sudo apt install nginx
 # Installing Node
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
+echo "Finished installing packages"
 
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
+echo "Configuring NGINX"
+# Setup ufw
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 22/tcp
+sudo ufw allow 'Nginx Full'
+
+# Create SSL Certificate (Will require user input)
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
+# Copy config files
+sudo cp self-signed.conf /etc/nginx/snippets/self-signed.conf
+sudo cp ssl-params.conf /etc/nginx/snippets/ssl-params.conf
+sudo cp default.conf /etc/nginx/sites-available/default
+sudo cp nginx.conf /etc/nginx/nginx.conf
+
+# Restart NGINX
+sudo systemctl restart nginx
+echo "Finshed configuring NGINX"
+
+echo "Installing npm packages"
+# Install project dependencies
+npm install
+echo "Finished installing npm packages"
 
 
-sudo npm -g install create-react-app
+echo "Starting server"
+npm start
+
+# Don't think this is needed
+# sudo npm cache clean -f
+# sudo npm install -g n
+# sudo n stable
+
+# sudo npm -g install create-react-app
